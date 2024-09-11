1d73a9eb71cb6eb77445f46589920782e264afa7
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2024-09-12 00:33:15 +0530

    FInally

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,25 +1,8 @@
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+const body = document.body;
 
-let navbar = document.querySelector(".navigation");
-
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-};
+document.getElementById("modeSwitch").addEventListener("change", () => {
+  body.classList.toggle("dark-mode");
+});
 
 const words = [
   "Student.", 
